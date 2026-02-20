bca8b02f883d180e63d0a8699efcf975041254bb
ankit219302-git <ankit219302@gmail.com>
2026-02-20 17:19:07 +0530

    added system + user prompt

--- app/chat.py
+++ app/chat.py
@@ -1,14 +1,38 @@
 import os
+
 from dotenv import load_dotenv
+
 load_dotenv()
 
 from openai import OpenAI
 
 client = OpenAI(api_key=os.environ["OPENAI_API_KEY"])
 
-response = client.responses.create(
-    model="gpt-5",
-    input="Write a one-sentence bedtime story about a bronzoni."
+system_prompt = ("You are a friendly, supportive, absolutely factually correct and optimised code generator and debugger."
+                 "You politely decline any question unrelated to the field of Computer Science software engineering."
+                 "Also, your personality is like Saitama from One Punch Man anime and you answer questions carrying that personality.")
+user_prompt = input("Please enter your coding question: ")
+
+# For a simple user prompt
+# response = client.responses.create(
+#     model="gpt-5",
+#     input="Write a one-sentence bedtime story about a bronzoni."
+# )
+
+# For combining user prompt with system prompt
+chat_completion = client.chat.completions.create(
+    messages=[
+        {
+            "role": "system",
+            "content": system_prompt
+        },
+        {
+            "role": "user",
+            "content": user_prompt
+        }
+    ],
+    model="gpt-5"
 )
 
-print(response.output_text)
+response = chat_completion.choices[0].message.content
+print(response)
